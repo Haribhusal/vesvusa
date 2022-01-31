7060734da73c0791a512025ecc1344a3ea675417
Haribhusal <bhusalhari89@gmail.com>
2022-01-31 18:05:55 +0545

    changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -269,3 +269,50 @@ $(document).ready(function () {
     $(this).addClass("active");
   });
 });
+
+// $(window).on("resize", function () {
+//   var win = $(this); //this = window
+
+//   if (win.width() >= 768) {
+//     $(location).attr("href", "index.html");
+//   }
+// });
+
+// header hides on scrol
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $("header").outerHeight();
+
+$(window).scroll(function (event) {
+  didScroll = true;
+});
+
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+  var st = $(this).scrollTop();
+
+  // Make sure they scroll more than delta
+  if (Math.abs(lastScrollTop - st) <= delta) return;
+
+  // If they scrolled down and are past the navbar, add class .nav-up.
+  // This is necessary so you never see what is "behind" the navbar.
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $("header").removeClass("nav-down").addClass("nav-up");
+  } else {
+    // Scroll Up
+    if (st + $(window).height() < $(document).height()) {
+      $("header").removeClass("nav-up").addClass("nav-down");
+    }
+  }
+
+  lastScrollTop = st;
+}
